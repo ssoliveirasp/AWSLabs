f5e0a7f35c5b9fd86d217f93f31d3397c7c0d474
ssoliveirasp <ssoliveira@gmail.com>
2022-12-12 17:03:00 -0300

    Create ForEach_CreateItemsOrItem.py

---
+++ API/ApiGateway/IntegrationType/LambdaFunction/ForEach_CreateItemsOrItem.py
@@ -0,0 +1,19 @@
+# static list of items
+items = [
+  {"ID": 1, "name": "Pen", "price": 2.5},
+  {"ID": 2, "name": "Pencil", "price": 1.5},
+  {"ID": 3, "name": "Highlighter", "price": 3.0},
+  {"ID": 4, "name": "Ruler", "price": 5.0},
+]
+def lambda_handler(event, context):
+  print("Event: %s" % event) # log event data
+  ID = event.get("ID") # extract ID 
+  # list case
+  if not ID:
+    return items
+  # ID case
+  found = [item for item in items if item["ID"] == ID]
+  if found:
+    return next(iter(found))
+  # nothing was found
+  raise Exception("NotFoundError")
